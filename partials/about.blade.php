4748518af992725f4865babc0fc7ec7fc5f6f975
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-26 11:46:15 -0400

    Adding contact details

---
+++ partials/about.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+  <div class="content">
+    <div class="text-center">
+      <h2 class="text-center">Contact Us</h2>
+      <hr class="separation-line blue-bg"/>
+
+      <p style="font-size:18px; margin-top:20px;" align="left">
+      Kevin Ellett - Gateway PI and Research Scientist <br>
+      Email: kmellett@indiana.edu <br>
+      Phone: (812) 856-3671
+      <a href="https://igws.indiana.edu/staff.cfm?user=kmellett"> Kevin's Staff Profile </a>
+      The Indiana Geological and Water Survey <br>
+      611 N. Walnut Grove St. <br>
+      Bloomington, IN 47405-2208 <br>
+      </p>
+
+
+
+    </div>
+  </div>
+</div>
