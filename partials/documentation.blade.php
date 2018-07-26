dc7ee375f68bd9b101135cec3c0dd68b983c19c2
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-26 11:23:48 -0400

    Addding documentation for PEARC18 hackathon

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -2,8 +2,8 @@
   <h2 class="text-center">Training and Documentation</h2>
     <hr class="separation-line blue-bg"/>
 
-        <h4>Instruction for SimCCS Gateway Hackathon at PEARC18</h4>
-          <h5>Instructions</h5>
+        <h3>Instruction for SimCCS Gateway Hackathon at PEARC18</h3>
+          <h4>Instructions</h4>
            <ul style="font-size:20px; list-style-type:square">
              <li><a href="/themes/iu-geosurvey-gateway-theme/assets/img/PEARC18_SimCCS_guide.pdf" download>PEARC18 SimCCS Gateway Guide</a></li>
              <li>Download .geojson files for the hackathon</li>
